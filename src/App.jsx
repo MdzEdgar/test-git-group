c273f6bc3c07c12b0cf66ed09407e2dce04d2210
Esmeralda Camacho <esmeraldacamachob@gmail.com>
2023-02-12 21:10:40 -0500

    esmeralda

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Esmeralda from './components/Esmeralda'
 import Jose from './components/Jose'
 import Misael from './components/Misael'
 
+
 function App() {
 
   return (
-    <div className="App flex gap-6 min-h-screen justify-center items-center ">
+    <div className="App flex gap-6 min-h-screen justify-center items-center bg-black text-white">
       <Edgar />
       <Esmeralda />
       <Jose />
--- src/components/Esmeralda.jsx
+++ src/components/Esmeralda.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Esmeralda = () => {
   return (
-    <div>Esmeralda</div>
+    <div className="bg-[#FECDD3]  p-8 rounded-md pt"> 
+      <h1 className='uppercase text-[30px] '>Esmeralda</h1>
+       <ul className='list-none text-center font-bold '>  
+       <li>Bailar</li>
+       <li>Comer</li>
+       <li>Viajar</li>
+       <li>Ir de compras</li>
+       </ul> 
+    </div>
   )
 }
 
