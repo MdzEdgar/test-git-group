1d874f2e38bb87d52ad6e28c60ea44be08d90093
Camilo Usma <camilo.usma.2017@gmail.com>
2023-02-12 21:13:05 -0500

    styled camilo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Camilo from './components/Camilo'
 import Edgar from './components/Edgar'
 import Esmeralda from './components/Esmeralda'
 import Jose from './components/Jose'
@@ -7,7 +8,9 @@ import Misael from './components/Misael'
 function App() {
 
   return (
-    <div className="App flex gap-6 min-h-screen justify-center items-center ">
+    <div className="App flex gap-6 min-h-screen justify-center items-center 
+    bg-black text-white">
+      <Camilo />
       <Edgar />
       <Esmeralda />
       <Jose />
---
+++ src/components/Camilo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Camilo = () => {
+  return (
+    <div className='bg-[#fff] p-10 rounded-md text-black text-center'>
+        <h1 className='uppercase text-[25px] font-bold '>Camilo</h1>
+        <ul className='list-none'>
+            <li>Comer</li>
+            <li>Estudiar</li>
+            <li>Rodar en moto</li>
+            <li>Escuchar música</li>
+        </ul>
+        </div>
+  )
+}
+
+export default Camilo
